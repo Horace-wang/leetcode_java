de9a43e8c1824e3d6c632f18bb497cfeb08cbfe1
horce1024@gmail.com <horce1024@gmail.com>
2022-01-20 15:31:04 +0800

    昨天的题因为生病没做，一次通过

---
+++ src/ContainsNearbyDuplicate219.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * 2022/1/19
+ * PACKAGE_NAME
+ * horace
+ */
+public class ContainsNearbyDuplicate219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                Integer under = map.get(nums[i]);
+                int fi = under - i;
+                if (Math.abs(fi) <= k) {
+                    return true;
+                }
+                map.put(nums[i], i);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
